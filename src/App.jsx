0fa20d322c63bc0993d410f74adee7fd85699b38
Maryna <89296066105marina@gmal.com>
2025-02-25 15:18:23 +0100

    rfc

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import "./App.css";
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-const HomePage = lazy(() => import("./pages/HomePage.jsx"));
-const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage.jsx"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage.jsx"));
-const NotFound = lazy(() => import("./pages/NotFound.jsx"));
+// const HomePage = lazy(() => import("./pages/HomePage.jsx"));
+// const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage.jsx"));
+// const MoviesPage = lazy(() => import("./pages/MoviesPage.jsx"));
+// const NotFound = lazy(() => import("./pages/NotFound.jsx"));
+const 
+
+
 function App() {
   return (
     <>
       <Suspense fallback={<div>Зачекайте...</div>}>
-        <Routes>
+      
+        {/* <Routes>
           
           <Route path="/" element={<HomePage />} />
           <Route path="/details" element={<MovieDetailsPage />} />
           <Route path="/movie" element={<MoviesPage />} />
           <Route path="*" element={<NotFound />} />
-        </Routes>
+        </Routes> */}
       </Suspense>
     </>
   );
---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,9 @@
+const MovieList = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default MovieList;
